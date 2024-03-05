d746279a2f7a8a81497d6cdc48ef1647293b4035
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-03-05 10:01:50 -0300

    feat: Add Button component - restyle

--- App.tsx
+++ App.tsx
@@ -4,21 +4,21 @@ import { Text } from './src/components/Text/Text';
 import { Button } from './src/components/Button/Button';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
+import { Box } from './src/components/Box/Box';
 
 function App(): React.JSX.Element {
 
 
   return (
     <ThemeProvider theme={theme}>
-
       <SafeAreaView>
         <Text preset="headingLarge" bold>
           Coffstack
         </Text>
-
-        <View style={{ margin: 24 }}>
-          <Button title='Entrar' />
-        </View>
+        <Box style={{ margin: 24 }}>
+          <Button title='Entrar' marginBottom='s14' backgroundColor='carrotSecondary' />
+          <Button title='Entrar' loading />
+        </Box>
       </SafeAreaView>
     </ThemeProvider>
   );
