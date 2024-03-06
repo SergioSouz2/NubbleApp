fde687682d9976309c0dced2bb1bf4f4ae624e67
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-03-06 16:51:36 -0300

    :sparkles: feat: Icon Component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { Button } from './src/components/Button/Button';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import { Box } from './src/components/Box/Box';
+import { EyeOffIcon } from './src/assets/icons/EyeOffIcon';
+import { EyeOnIcon } from './src/assets/icons/EyeOnIcon';
+import { Icon } from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
 
@@ -21,6 +24,10 @@ function App(): React.JSX.Element {
             <Button preset='outline' title='Outline' marginBottom='s12' />
             <Button title='Loading' loading marginBottom='s12' />
             <Button disabled title='Disabled' />
+
+            <Icon name='eyeOn' size={50} color='backgroundContrast' />
+            <Icon name='eyeOff' size={100} color='carrotSecondary' />
+
           </Box>
         </View>
       </SafeAreaView>
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { ActivityIndicatorProps, ActivityIndicator as RNActivityIndicator } from 'react-native'
-import { Theme, ThemeColors } from "../../theme/theme";
-import { useTheme } from "@shopify/restyle";
+import { ThemeColors } from "../../theme/theme";
+import { useAppTheme } from "../../hooks/useAppTheme";
 
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
@@ -11,7 +11,7 @@ interface Props extends Omit<ActivityIndicatorProps, 'color'> {
 
 export function ActivityIndicator({ color }: Props) {
 
-  const { colors } = useTheme<Theme>();
+  const { colors } = useAppTheme();
   return (
     <RNActivityIndicator color={colors[color]} />
   )
