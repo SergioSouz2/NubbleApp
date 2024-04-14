c38576f40cbbd15ea9c4b899c0129caca9474d9a
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-04-14 02:09:33 -0300

    :sparkles: feat: Post domain

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -4,19 +4,19 @@ import {Button, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
-  return (
-    <Screen>
-      <Text preset="headingLarge"> HomeScreen </Text>
-      <Button
-        title="Settings"
-        onPress={() => navigation.navigate('SettingsScreen')}
-      />
+   return (
+      <Screen>
+         <Text preset="headingLarge"> HomeScreen </Text>
+         <Button
+            title="Settings"
+            onPress={() => navigation.navigate('SettingsScreen')}
+         />
 
-      <Button
-        title="FavoriteScreen"
-        marginTop="s14"
-        onPress={() => navigation.navigate('FavoriteScreen')}
-      />
-    </Screen>
-  );
+         <Button
+            title="FavoriteScreen"
+            marginTop="s14"
+            onPress={() => navigation.navigate('FavoriteScreen')}
+         />
+      </Screen>
+   );
 }
--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -4,17 +4,17 @@ import {Button, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function SettingsScreen({navigation}: AppScreenProps<'SettingsScreen'>) {
-  return (
-    <Screen canGoBack>
-      <Text preset="headingLarge">SettingScreen</Text>
-      <Button
-        title="New Post"
-        onPress={() =>
-          navigation.navigate('AppTabNavigator', {
-            screen: 'NewPostScreen',
-          })
-        }
-      />
-    </Screen>
-  );
+   return (
+      <Screen canGoBack>
+         <Text preset="headingLarge">SettingScreen</Text>
+         <Button
+            title="New Post"
+            onPress={() =>
+               navigation.navigate('AppTabNavigator', {
+                  screen: 'NewPostScreen',
+               })
+            }
+         />
+      </Screen>
+   );
 }
