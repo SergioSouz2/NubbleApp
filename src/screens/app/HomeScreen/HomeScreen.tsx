c63c58dda85b3cf78be27541b0e1182476be1222
SergioSouz2 <sergio.tbl0123@gmail.com>
2024-04-25 17:01:22 -0300

    :recycle: refactor: Caso de uso useCases -> Camada de Aplicação

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,43 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FlatList, ListRenderItemInfo, StyleProp, ViewStyle } from 'react-native';
 
 import { PostItem, Screen } from '@components';
 
 import { AppTabScreenProps } from '@routes';
 
-import { Post, postService } from '@domain';
+import { Post, usePostList } from '@domain';
 
 import { HomeHeader } from './components/HomeHeader';
 import { HomeEmpty } from './components/HomeEmpty';
 
 export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
-
-   const [postList, setPostList] = useState<Post[]>([]);
-   const [loading, setLoading] = useState(true);
-   const [error, setError] = useState<boolean | null>(null);
-
-
-   async function fetchData() {
-      try {
-         setError(null)
-         setLoading(true)
-         const list = await postService.getList()
-         setPostList(list)
-      } catch (error) {
-         setError(true)
-      } finally {
-         setLoading(false)
-      }
-   }
-
+   const { error, loading, postList, refetch } = usePostList()
 
    function renderItem({ item }: ListRenderItemInfo<Post>) {
       return <PostItem post={item} />;
    }
 
-   useEffect(() => {
-      fetchData()
-   }, []);
+
 
    return (
       <Screen style={$screen}>
@@ -48,7 +28,7 @@ export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
             renderItem={renderItem}
             contentContainerStyle={{ flex: postList.length === 0 ? 1 : undefined }}
             ListHeaderComponent={<HomeHeader />}
-            ListEmptyComponent={<HomeEmpty refetch={fetchData} error={error} loading={loading} />}
+            ListEmptyComponent={<HomeEmpty refetch={refetch} error={error} loading={loading} />}
          />
       </Screen>
    );
